e7bac89aeb3d81e10578d719485c92565c3c4502
Gabi <engs-gabrielliferreira@camporeal.edu.br>
2026-04-15 16:54:35 -0300

    Site base

---
+++ assets/perguntas.js
@@ -0,0 +1,17 @@
+criar_cartao(
+    "Programação",
+    "Onde fica o link do CSS?",
+    "No head"
+)
+
+criar_cartao(
+    "futebol",
+    "Qual o campeão da Livbertadores de 2024?",
+    "Botafogo"
+)
+
+criar_cartao(
+    "futebol",
+    "Qual o campeão da Livbertadores de 2024?",
+    "Botafogo"
+)
